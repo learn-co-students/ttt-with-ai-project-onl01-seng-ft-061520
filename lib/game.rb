d3da410bf57b190908d50e94a48cd5285d6f5933
Shirlen Detablan <slen.detablan@gmail.com>
2020-07-06 15:29:59 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -19,3 +19,57 @@ class Game
     @player_1 = player_1
     @player_2 = player_2
   end
+  
+  def current_player
+    @board.turn_count % 2 == 0 ? player_1 : player_2
+  end
+  
+  def won?
+    WIN_COMBINATIONS.detect do |combo|
+      @board.cells[combo[0]] == @board.cells[combo[1]] &&
+      @board.cells[combo[1]] == @board.cells[combo[2]] &&
+      @board.taken?(combo[0]+1)
+    end
+  end
+  
+  def draw?
+    @board.full? && !won?
+  end 
+  
+  def over?
+    won? || draw?
+  end
+  
+  def winner
+    if winning_combo = won?
+      @winner = @board.cells[winning_combo.first]
+    end 
+  end
+  
+  def turn
+    player = current_player
+    current_move = player.move(@board)
+    if !@board.valid_move?(current_move)
+      turn
+    else
+      puts "Turn: #{@board.turn_count+1}\n"
+      @board.display
+      @board.update(current_move, player)
+      puts "#{player.token} moved #{current_move}"
+      @board.display
+      puts "\n\n"
+    end
+  end
+  
+  def play 
+    while !over?
+      turn
+    end
+    if won?
+      puts "Congratulations #{winner}!"
+    elsif draw?
+      puts "Cat's Game!"
+    end
+  end
+  
+end 
